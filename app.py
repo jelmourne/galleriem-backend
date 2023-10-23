c924adb4f751c27863ef8c7625a236ed33f03c02
Flo B <florjanblakaj@hotmail.com>
2023-10-23 19:39:18 -0400

    Updated database

--- app.py
+++ app.py
@@ -2,37 +2,85 @@ import os
 from flask import Flask, jsonify, request
 from models import db
 from models import *
+
 os.environ["FLASK_ENV"] = "development"
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-@app.route("/", methods=["GET"])
-def welcome():
-    new_customer = Customer(
-    customerName="test Doe",
-    address="Toronto Street",
-    email="test@test.com",
-    creditCardInfo="1111-1111-1111-3456",
-    shippingInfo="456 111 Street"
-)
-    # Add the new customer to the session and commit the transaction
+@app.route("/add/admin", methods=["GET"])
+def add_admin():
+    new_admin = Administrator(adminUsername="adminUser", password="securePassword123")
+    db.session.add(new_admin)
+    db.session.commit()
+    return jsonify(message="New admin added"), 201
+
+@app.route("/add/product", methods=["GET"])
+def add_product():
+    new_product = Product(
+        productType="Shirt",
+        productDescription="A cool shirt for summer.",
+        productSize=["S", "M", "L", "XL"],
+        productColor=["Red", "Blue", "Green"]
+    )
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify(message="New product added"), 201
+
+@app.route("/add/customer", methods=["GET"])
+def add_customer():
+    new_customer = Customer()
     db.session.add(new_customer)
     db.session.commit()
+    return jsonify(message="New customer added"), 201
 
-    return f"Created Customer: {new_customer.customerName}"
+@app.route("/add/order", methods=["GET"])
+def add_order():
+    import datetime
+    new_order = Order(
+        customerId=1,
+        customerName="John Doe",
+        address="123 Elm St, Springfield",
+        email="john.doe@example.com",
+        creditCardInfo="1111-2222-3333-4444",
+        shippingInfo="123 Elm St, Springfield",
+        orderDate=datetime.date.today(),
+        status="Processing",
+        total=99.99
+    )
+    db.session.add(new_order)
+    db.session.commit()
+    return jsonify(message="New order added"), 201
 
-@app.route("/Product", methods=["GET"])
-def getProducts():
-    return "dsadsa"
+@app.route("/add/cart", methods=["GET"])
+def add_cart():
+    new_cart = ShoppingCart(
+        total=49.99,
+        customerId=1,
+        itemID=1
+    )
+    db.session.add(new_cart)
+    db.session.commit()
+    return jsonify(message="New cart added"), 201
 
-@app.route("/Product/<id>", methods=["GET"])
-def getProduct(id):
-    return id
+@app.route("/add/cart-item", methods=["GET"])
+def add_cart_item():
+    new_cart_item = ShoppingCartItem(
+        refProductID=1,
+        cartID=1,
+        quantity=2,
+        customSize="L",
+        customColor="Blue",
+        customDesign="Design123"
+    )
+    db.session.add(new_cart_item)
+    db.session.commit()
+    return jsonify(message="New cart item added"), 201
+
 
 if __name__ == "__main__":
     with app.app_context():  
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PickleType
 
 db = SQLAlchemy()
 
@@ -10,23 +11,31 @@ class Product(db.Model):
     productId = db.Column(db.Integer, primary_key=True)
     productType = db.Column(db.String(255))
     productDescription = db.Column(db.Text)
-    productSize = db.Column(db.String(50))
-    productColor = db.Column(db.String(50))
+    productSize = db.Column(PickleType)  # List of sizes
+    productColor = db.Column(PickleType)  # List of colors
 
 class Customer(db.Model):
     customerId = db.Column(db.Integer, primary_key=True)
+    def __repr__(self):
+        return f'<Customer {self.customerName} - Email: {self.email}>'
+
+class Orders(db.Model):
+    orderId = db.Column(db.Integer, primary_key=True)
+    customerId = db.Column(db.Integer, db.ForeignKey('customer.customerId'))
     customerName = db.Column(db.String(255), nullable=False)
     address = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     creditCardInfo = db.Column(db.String(255))
     shippingInfo = db.Column(db.Text)
-    def __repr__(self):
-        return f'<Customer {self.customerName} - Email: {self.email}>'
+    orderDate = db.Column(db.Date)
+    status = db.Column(db.String(50))
+    total = db.Column(db.Float)
 
 class ShoppingCart(db.Model):
     cartID = db.Column(db.Integer, primary_key=True)
-    total = db.Column(db.Float, default=0)
+    total = db.Column(db.Float)
     customerId = db.Column(db.Integer, db.ForeignKey('customer.customerId'))
+    itemID = db.Column(db.Integer, db.ForeignKey('shopping_cart_item.itemID'))
 
 class ShoppingCartItem(db.Model):
     itemID = db.Column(db.Integer, primary_key=True)
@@ -36,18 +45,6 @@ class ShoppingCartItem(db.Model):
     customSize = db.Column(db.String(50))
     customColor = db.Column(db.String(50))
     customDesign = db.Column(db.Text)
-
-class ShippingInfo(db.Model):
-    shippingId = db.Column(db.Integer, primary_key=True)
-    shippingAddress = db.Column(db.Text, nullable=False)
-    shippingRegion = db.Column(db.String(255))
-
-class Orders(db.Model):
-    orderId = db.Column(db.Integer, primary_key=True)
-    orderDate = db.Column(db.Date)
-    status = db.Column(db.String(50))
-    shippingId = db.Column(db.Integer, db.ForeignKey('shipping_info.shippingId'))
-    cartID = db.Column(db.Integer, db.ForeignKey('shopping_cart.cartID'))
 
 def init_app(app):
     db.init_app(app)
