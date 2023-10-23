46fe7afe84f32fa129e1e5fb6aa297f1802d24b1
Justin Elmourne <jelmourne@outlook.com>
2023-10-23 14:29:07 -0400

    First commit

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ import json
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+
+@app.route("/Product", methods=["GET"])
+def getProducts():
+    return "dsadsa"
+
+
+@app.route("/Product/<id>", methods=["GET"])
+def getProduct(id):
+    return id
+
+
+if __name__ == "__main__":
+    app.run()
